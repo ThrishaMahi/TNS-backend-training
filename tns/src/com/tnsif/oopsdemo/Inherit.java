1853b588011412dca6e342253e929a3e0094f8f9
ThrishaMahi <Thrisha Mahi@LAPTOP-F9KLSSQ3>
2025-05-04 16:50:42 +0530

    Inheritance

---
+++ tns/src/com/tnsif/oopsdemo/Inherit.java
@@ -0,0 +1,13 @@
+package com.tnsif.oopsdemo;
+
+public class Inherit {
+	void Hi() {
+		System.out.println("Say Hi!");
+	}
+
+}
+   class Hello extends Inherit{
+	   void sound() {
+		   System.out.println("Some sound!!");
+	   }
+   }
---
+++ tns/src/com/tnsif/oopsdemo/InheritMain.java
@@ -0,0 +1,13 @@
+package com.tnsif.oopsdemo;
+
+public class InheritMain {
+	public static void main(String args[]) {
+		
+	
+	Hello e = new Hello();
+	e.Hi();
+	e.sound();
+	}
+	
+
+}
