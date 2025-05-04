4c2aa1e94b665fb0d4c110789daa6f6b3498b38c
ThrishaMahi <Thrisha Mahi@LAPTOP-F9KLSSQ3>
2025-05-04 16:47:48 +0530

    Encapsulation

---
+++ tns/src/com/tnsif/oopsdemo/EncapsulationMain.java
@@ -0,0 +1,16 @@
+package com.tnsif.oopsdemo;
+
+public class EncapsulationMain {
+	public static void main(String args[]) {
+		
+	
+	Encapsulation e = new Encapsulation();
+	e.setName("Thrisha");
+	System.out.println(e.getName());
+	e.setId(101);
+	System.out.println(e.getId());
+	}
+	
+	
+
+}
