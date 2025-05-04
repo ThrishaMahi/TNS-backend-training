731117a7f499964bef6359015be56ffe20c5a534
ThrishaMahi <Thrisha Mahi@LAPTOP-F9KLSSQ3>
2025-05-04 16:51:56 +0530

    Compile-time Polymorphism

---
+++ tns/src/com/tnsif/oopsdemo/Polymorph.java
@@ -0,0 +1,14 @@
+package com.tnsif.oopsdemo;
+//Complile time - Overloading
+
+public class Polymorph {
+	public int add(int n1,int n2) {
+		return n1+n2;
+	}
+	public float add(float a1,float a2) {
+		return a1+a2;
+		
+	}
+	
+
+}
---
+++ tns/src/com/tnsif/oopsdemo/PolymorphMain.java
@@ -0,0 +1,10 @@
+package com.tnsif.oopsdemo;
+
+public class PolymorphMain {
+	public static void main(String args[]) {
+		Polymorph ob = new Polymorph();
+		System.out.println(ob.add(2, 98));
+		System.out.println(ob.add(45, 234));
+	}
+
+}
