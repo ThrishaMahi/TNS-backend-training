b36f3e1d03ff5579b8f24790f09c8bc57d7346fa
ThrishaMahi <Thrisha Mahi@LAPTOP-F9KLSSQ3>
2025-05-13 23:15:40 +0530

    Thread Synchronization

---
+++ tns/src/com/tnsif/ThreadDemo/ThreadSync.java
@@ -0,0 +1,13 @@
+package com.tnsif.ThreadDemo;
+
+public class ThreadSync {
+	synchronized public static void add(int n) throws InterruptedException {
+		
+		for(int i=0;i<4;i++) {
+			System.out.println(n+i);
+			Thread.sleep(1000);
+		}
+		
+	}
+	
+}
